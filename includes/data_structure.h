6dbfd2d1342b476df1f9985c3849eca921dcd23c
Abdulaziz Abduvakhobov <abdulaziz.yosk@gmail.com>
2022-08-20 04:07:22 +0300

    Added OS checker to Makefile

--- includes/data_structure.h
+++ includes/data_structure.h
@@ -1,33 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cub3d.h                                            :+:      :+:    :+:   */
+/*   data_structure.h                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aabduvak <aabduvak@42istanbul.com.tr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/08 07:51:17 by aabduvak          #+#    #+#             */
-/*   Updated: 2022/08/20 04:05:22 by aabduvak         ###   ########.fr       */
+/*   Created: 2022/08/20 04:03:11 by aabduvak          #+#    #+#             */
+/*   Updated: 2022/08/20 04:03:58 by aabduvak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CUB3D_H
-# define CUB3D_H
-
-# include <stdlib.h>
-# include <unistd.h>
-# include <limits.h>
-# include <stdbool.h>
-# include <stdio.h>
-# include <fcntl.h>
-# include <sys/wait.h>
-
-# include <../lib/minilibx/mlx.h>
-
-# include <libft.h>
-# include <ft_printf.h>
-# include <get_next_line.h>
-# include <macro.h>
-# include <data_structure.h>
-# include <functions.h>
+#ifndef DATA_STRUCTURE_H
+# define DATA_STRUCTURE_H
 
 #endif
--- includes/functions.h
+++ includes/functions.h
@@ -1,33 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cub3d.h                                            :+:      :+:    :+:   */
+/*   functions.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aabduvak <aabduvak@42istanbul.com.tr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/08 07:51:17 by aabduvak          #+#    #+#             */
-/*   Updated: 2022/08/20 04:05:22 by aabduvak         ###   ########.fr       */
+/*   Created: 2022/08/20 04:04:27 by aabduvak          #+#    #+#             */
+/*   Updated: 2022/08/20 04:05:02 by aabduvak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CUB3D_H
-# define CUB3D_H
-
-# include <stdlib.h>
-# include <unistd.h>
-# include <limits.h>
-# include <stdbool.h>
-# include <stdio.h>
-# include <fcntl.h>
-# include <sys/wait.h>
-
-# include <../lib/minilibx/mlx.h>
-
-# include <libft.h>
-# include <ft_printf.h>
-# include <get_next_line.h>
-# include <macro.h>
-# include <data_structure.h>
-# include <functions.h>
+#ifndef FUNCTIONS_H
+# define FUNCTIONS_H
 
 #endif
